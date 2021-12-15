950d8d984601956883dfdac6cc600d8a8f100f47
srinath ragalla <srinathragalla17@gmail.com>
2021-12-16 01:11:21 +0530

    object keys

---
+++ details of an object.js
@@ -0,0 +1,14 @@
+var student ={
+    name:"myself",
+    Age:20,
+        marks: {
+            english:95,
+            maths:85,
+            physics:90,
+        }
+    }
+    
+  //each member of an object is a key
+console.log(student.Age)
+console.log(student.marks.english)
+console.log(typeof student)
